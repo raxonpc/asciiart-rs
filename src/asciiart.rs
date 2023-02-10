4f5304459a31855a614f89f1e968e00637dee11d
raxonpc <tomeczekorlowski1@gmail.com>
2023-02-10 17:04:09 +0100

    Refactor

--- src/asciiart.rs
+++ src/asciiart.rs
@@ -1,7 +1,28 @@
-use leptonica_sys::{ PIX, pixRead, pixDestroy, pixRemoveColormap, pixConvert16To8, pixConvertRGBToGrayFast, REMOVE_CMAP_TO_GRAYSCALE };
+use leptonica_sys::{ PIX, pixRead, pixDestroy, pixGetWidth, pixGetHeight, pixConvertTo8, pixGetPixel, pixScaleToSize };
 use std::ffi::CString;
-use std::io::{Error, ErrorKind};
-use std::ptr::null;
+use crate::asciiart::ProcessingError::{CannotConvertToGrayscale, CannotGetPixel, InvalidFile, UnsupportedImageFormat};
+
+#[derive(Debug)]
+pub enum ProcessingError {
+    InvalidFile,
+    CannotConvertToGrayscale,
+    UnsupportedImageFormat,
+    CannotScale,
+    CannotGetPixel
+}
+
+impl std::fmt::Display for ProcessingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use ProcessingError::*;
+        match *self {
+            InvalidFile => write!(f, "File not found or file is not an image"),
+            CannotConvertToGrayscale => write!(f, "Could not convert file to grayscale"),
+            UnsupportedImageFormat => write!(f, "File is not supported"),
+            CannotScale => write!(f, "Could not scale the image"),
+            CannotGetPixel=> write!(f, "Could not extract a pixel from the image"),
+        }
+    }
+}
 
 struct PixWrapper {
     pix: *mut PIX
@@ -15,82 +36,94 @@ impl Drop for PixWrapper {
     }
 }
 
+fn grayscale_to_ascii(gray: u32) -> char {
+    const ASCII_SHADES: &str = " .:-=+*#%@";
+
+    let part = gray as f64 / 255.0;
+    let index = part * (ASCII_SHADES.len() - 1) as f64;
+
+    ASCII_SHADES.chars().nth(index as usize).unwrap()
+}
+
 impl PixWrapper {
-    fn new(file_name: &str) -> Result<Self, std::io::Error> {
+    fn new(file_name: &str) -> Result<Self, ProcessingError> {
         unsafe {
             let c_str = CString::new(file_name).unwrap();
 
-            unsafe {
-                let pix = pixRead(c_str.as_ptr());
-                if pix.is_null() {
-                    Err(std::io::Error::new(ErrorKind::Other, "Error reading file"))
-                } else {
-                    Ok(PixWrapper { pix })
-                }
+            let pix = pixRead(c_str.as_ptr());
+            if pix.is_null() {
+                Err(InvalidFile)
+            } else {
+                Ok(PixWrapper { pix })
             }
         }
     }
 
-    fn to_grayscale(&mut self) -> Result<(), String> {
+    fn to_grayscale(&mut self) -> Result<(), ProcessingError> {
         unsafe {
             match (*self.pix).d {
-                1 | 2 | 4 | 8 => {
-                    let pix = pixRemoveColormap(self.pix, REMOVE_CMAP_TO_GRAYSCALE as i32);
+                1 | 2 | 4 | 8 | 16 | 32 => {
+                    let pix = pixConvertTo8(self.pix, 0);
                     if pix.is_null() {
-                        Err("Failed removing colormap from the image".to_string())
+                        Err(CannotConvertToGrayscale)
                     } else {
                         pixDestroy(&mut self.pix);
                         self.pix = pix;
                         Ok(())
                     }
                 },
-                16 => {
-                    let mut pix = pixConvert16To8(self.pix, 0);
-                    if pix.is_null() {
-                        return Err("Failed converting 16bpp image to 8bpp image".to_string());
-                    }
-
-                    pixDestroy(&mut pix);
-                    let pix = pixRemoveColormap(pix, REMOVE_CMAP_TO_GRAYSCALE as i32);
-                    if pix.is_null() {
-                        Err("Failed removing colormap from the image".to_string())
-                    } else {
-                        pixDestroy(&mut self.pix);
-                        self.pix = pix;
-                        Ok(())
-                    }
-                },
-                32 => {
-                    let pix = pixConvertRGBToGrayFast(self.pix);
-                    if pix.is_null() {
-                        Err("Failed removing colormap from the image".to_string())
-                    } else {
-                        pixDestroy(&mut self.pix);
-                        self.pix = pix;
-                        Ok(())
-                    }
-                }
-                _ => todo!()
+                _ => Err(UnsupportedImageFormat)
             }
         }
     }
-}
 
-fn read_pix(file_name: &str) -> Result<*mut PIX, std::io::Error> {
-    let c_str = CString::new(file_name).unwrap();
+    fn scale(&mut self, ratio: f64) -> Result<(), ProcessingError> {
+        unsafe {
+            let pix = pixScaleToSize(self.pix,
+                                     (pixGetWidth(self.pix) as f64 * ratio) as i32,
+                                     (pixGetHeight(self.pix)as f64 * ratio) as i32);
+            if pix.is_null() {
+                Err(ProcessingError::CannotScale)
+            } else {
+                pixDestroy(&mut self.pix);
+                self.pix = pix;
+                Ok(())
+            }
+        }
+    }
 
-    unsafe {
-        let pix = pixRead(c_str.as_ptr());
-        if pix.is_null() {
-            Err(std::io::Error::new(ErrorKind::Other, "Error reading file"))
-        } else {
-            Ok(pix)
+    fn to_string(&self) -> Result<String, ProcessingError> {
+        let mut output = String::new();
+
+        unsafe {
+            let width = pixGetWidth(self.pix);
+            let height = pixGetHeight(self.pix);
+            output.reserve_exact((width * height) as usize);
+
+            for row in 0..height {
+                for column in 0..width {
+                    let mut out_var: u32 = 0;
+                    let status = pixGetPixel(self.pix, column as i32, row as i32, std::ptr::addr_of_mut!(out_var));
+                    if status == 1 {
+                        return Err(CannotGetPixel)
+                    }
+
+                    output.push(grayscale_to_ascii(out_var));
+                }
+                output.push('\n');
+            }
+            Ok(output)
         }
     }
 }
 
+pub fn to_ascii_art(file_name: &str, scale: Option<f64>) -> Result<String, ProcessingError> {
+    let mut wrapper = PixWrapper::new(file_name)?;
+    wrapper.to_grayscale()?;
+    match scale {
+        Some(val) => wrapper.scale(val)?,
+        None => wrapper.scale(1.0)?
+    }
 
-pub fn to_ascii_art(file_name: &str) -> String {
-    PixWrapper::new(file_name).unwrap().to_grayscale().unwrap();
-    "Dupa".to_string()
+    wrapper.to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use std::{env};
-use std::error::Error;
 
 mod asciiart;
 
-fn main() {
+use asciiart::{ ProcessingError };
+
+fn main() -> Result<(), ProcessingError>{
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("usage: asciiart [file ...]");
-        return ();
+        eprintln!("usage: asciiart file scale");
+        return Ok(());
     } else {
-        for arg in &args[1..] {
-            asciiart::to_ascii_art(arg);
+        let mut scale: f64 = 1.0;
+        if let Some(s) = args.get(2) {
+            scale = s.parse().unwrap();
         }
+        let output = asciiart::to_ascii_art(args.get(1).unwrap(), Some(scale))?;
+        println!("{}", &output);
     }
-
+    Ok(())
 }
